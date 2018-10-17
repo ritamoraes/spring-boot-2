4e7cd14bdb0443e02af6392a9ba32fd0854354ae
Rita Moraes <ritac.moraes@gmail.com>
2018-10-17 11:49:02 -0300

    Rodando mas com problemas no h2

---
+++ src/main/java/com/tw/springboot28m/mcourse/events/GitHubProject.java
@@ -0,0 +1,4 @@
+package com.tw.springboot28m.mcourse.events;
+
+public class GitHubProject {
+}
---
+++ src/main/java/com/tw/springboot28m/mcourse/events/GitHubProjectRepository.java
@@ -0,0 +1,4 @@
+package com.tw.springboot28m.mcourse.events;
+
+public interface GitHubProjectRepository {
+}
